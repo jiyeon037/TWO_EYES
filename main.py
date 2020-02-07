e2bdfc77a4db62507e1b4bf7dff1e31a57cbf7af
jiyeon037 <jiyeon7269@naver.com>
2020-02-07 11:37:06 +0900

    modify map size

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from flask_bootstrap import Bootstrap
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
 #import folium
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 Bootstrap(app)
+
+# you can set key as config
+app.config['GOOGLEMAPS_KEY'] = ""
+
+# you can also pass key here
+GoogleMaps(
+    app,
+)
 
 
 @app.route('/')
 def index():
-   # start_coords = (46.9540700, 142.7360300)
-   # folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return render_template('index.html')
+
+    sndmap = Map(
+        style="height:450px;width:1200px;",
+        identifier="sndmap",
+        varname="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419), (37.4500, -122.1350), (37.4300, -122.1400, "Hello World")]
+    )
+        
+    return render_template('index.html', sndmap=sndmap, GOOGLEMAPS_KEY=request.args.get('apikey'))
+
+
 
 '''
 @app.route('/test', method=['GET', 'POST'])
